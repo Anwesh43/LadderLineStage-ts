674b523f28820fd89ce38e5eb6637ba7a46724ed
Anwesh43 <anweshthecool0@gmail.com>
2018-09-01 17:08:03 +0530

    adding LadderLine class

--- LadderLineStage.ts
+++ LadderLineStage.ts
@@ -134,3 +134,25 @@ class LLNode {
         return this
     }
 }
+
+class LadderLine {
+    curr : LLNode = new LLNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
